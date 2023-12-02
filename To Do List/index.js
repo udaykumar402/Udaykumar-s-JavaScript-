27d7c53309f02785c240f78ab5119e32fcf7c489
UDAYKUMAR <uday402403@gmail.com>
2023-12-02 22:31:00 +0530

    comments added fot js code

--- To Do List/index.js
+++ To Do List/index.js
@@ -1,41 +1,66 @@
+// Get references to the input box and list container in the HTML
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+// Function to add a new task
 function addTask() {
+  // Check if the input box is empty
   if (inputBox.value === "") {
     alert("You must write something!");
   } else {
+    // Create a new list item and set its inner HTML to the input value
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
+
+    // Append the new list item to the list container
     listContainer.appendChild(li);
+
+    // Create a close button (span) for the list item
     let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
+    span.innerHTML = "\u00d7"; // Unicode for '×' (close symbol)
+
+    // Append the close button to the list item
     li.appendChild(span);
   }
+
+  // Clear the input box after adding a task
   inputBox.value = "";
+
+  // Save the updated task list to local storage
   saveData();
 }
 
+// Event listener for clicks on the list container
 listContainer.addEventListener(
   "click",
   function (e) {
+    // Check if the clicked element is a list item (LI)
     if (e.target.tagName === "LI") {
+      // Toggle the 'checked' class for the clicked list item
       e.target.classList.toggle("checked");
+
+      // Save the updated task list to local storage
       saveData();
     } else if (e.target.tagName === "SPAN") {
+      // If the clicked element is a close button (SPAN), remove its parent list item
       e.target.parentElement.remove();
+
+      // Save the updated task list to local storage
       saveData();
     }
   },
   false
 );
 
+// Function to save the current task list to local storage
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
+// Function to load and display tasks from local storage
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
 }
 
+// Show tasks when the page is loaded or refreshed
 showTask();
--- Weather Report App/index.js
+++ Weather Report App/index.js
@@ -1,59 +1,54 @@
+// OpenWeatherMap API key and endpoint
 const apikey = "4bfb1f064548e3fda7d5d2d041283505";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
+// DOM elements
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
+const card = document.querySelector(".card");
 
-var card = document.querySelector(".card");
-
+// Function to fetch and display weather information
 async function checkweather(city) {
+  // Fetch weather data from the OpenWeatherMap API
   const response = await fetch(apiUrl + city + `&appid=${apikey}`);
 
+  // Check if the city is not found (status code 404)
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
-    // document.querySelector(".weather").style.display = "none";
   } else {
+    // Parse the JSON response
     var data = await response.json();
     console.log(data);
 
+    // Update DOM elements with weather information
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "kmph";
 
+    // Set weather icon based on weather conditions
     if (data.weather[0].main == "Clouds") {
       weatherIcon.src = "images/clouds.png";
-      //   card.style.backgroundColor = "#D3D3D3";
-      //   card.style.backgroundImage =
-      //     "url('https://community.adobe.com/legacyfs/online/1516782_Clouds.gif')";
     } else if (data.weather[0].main == "Clear") {
       weatherIcon.src = "images/clear.png";
-      //   card.style.backgroundColor = "#87CEEB";
-      //   card.style.backgroundImage =
-      //     "url('https://media.istockphoto.com/id/482974538/photo/blue-sky-with-cloud.jpg?s=612x612&w=0&k=20&c=_V2RUCJmTj6G8ypV4x8420MkUji6YS6wTu3sq8BfWGY=')";
     } else if (data.weather[0].main == "Rain") {
       weatherIcon.src = "images/rain.png";
-      //   card.style.backgroundColor = "#696969";
-      //   card.style.backgroundImage =
-      //     "url('https://www.gifimages.pics/images/quotes/english/general/rain-love-gif-two-frogs-52650-208792.gif')";
     } else if (data.weather[0].main == "Drizzle") {
       weatherIcon.src = "images/drizzle.png";
-      //   card.style.backgroundColor = "#ADD8E6";
-      //   card.style.backgroundImage =
-      //     "url('https://cdn.pixabay.com/animation/2023/03/05/12/05/12-05-54-62_512.gif')";
     } else if (data.weather[0].main == "Mist") {
       weatherIcon.src = "images/mist.png";
-      //   card.style.backgroundColor = "#F5F5F5";
-      //   card.style.backgroundImage = "url('https://i.gifer.com/GCuj.gif')";
     }
 
+    // Hide error message if weather data is successfully retrieved
     document.querySelector(".error").style.display = "none";
   }
 }
 
+// Event listener for the search button
 searchBtn.addEventListener("click", () => {
+  // Call the checkweather function with the value from the search input
   checkweather(searchBox.value);
 });
